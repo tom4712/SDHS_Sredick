d460bb63a15d3fc0523bca7b0cddd0224d51217a
tom4712 <tom4712@naver.com>
2018-03-06 17:05:15 +0900

    as

--- app/src/main/java/kr/hs/sdh/fitbit/sdhs_sredick/RegisterActivity.java
+++ app/src/main/java/kr/hs/sdh/fitbit/sdhs_sredick/RegisterActivity.java
@@ -3,11 +3,19 @@ package kr.hs.sdh.fitbit.sdhs_sredick;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
     }
+
 }
